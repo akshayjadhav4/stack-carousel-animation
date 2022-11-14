dcd4ab8ccb2b70c289a3aa973c46d4830db6b1e4
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-11-14 14:13:59 +0530

    stacking slides

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, FlatList, Image, StyleSheet, View } from "react-native";
+import { SEVEN_WONDERS } from "./data/SevenWonders";
+
+const { width } = Dimensions.get("window");
+
+const SLIDE_WIDTH = width * 0.8;
+const SLIDE_HEIGHT = SLIDE_WIDTH * 1.6;
+const OFFSET_SPACING = 10;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={SEVEN_WONDERS}
+        horizontal
+        inverted
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: "center",
+          padding: OFFSET_SPACING * 2,
+        }}
+        scrollEnabled={false}
+        removeClippedSubviews={false}
+        keyExtractor={(item) => String(item.key)}
+        renderItem={({ item, index }) => {
+          return (
+            <View
+              style={[
+                styles.slideContainer,
+                { zIndex: SEVEN_WONDERS.length - index },
+              ]}
+            >
+              <Image source={{ uri: item.image }} style={styles.slideImage} />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +42,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    paddingVertical: 20,
+  },
+  slideImage: {
+    width: SLIDE_WIDTH,
+    height: SLIDE_HEIGHT,
+  },
+  slideContainer: {
+    position: "absolute",
+    left: -SLIDE_WIDTH / 2,
   },
 });
