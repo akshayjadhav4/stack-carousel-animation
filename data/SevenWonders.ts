cf85fd6f8ed56bbe90a950855342932581a414fa
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-11-14 14:13:41 +0530

    data

---
+++ data/SevenWonders.ts
@@ -0,0 +1,66 @@
+interface Wonder {
+  key: number;
+  name: string;
+  image: string;
+  imageAuthor: string;
+  location: string;
+}
+
+export const SEVEN_WONDERS: Wonder[] = [
+  {
+    key: 0,
+    name: "Taj Mahal",
+    image:
+      "https://images.pexels.com/photos/12446203/pexels-photo-12446203.jpeg?cs=srgb&dl=pexels-laura-barrag%C3%A1n-12446203.jpg&fm=jpg",
+    imageAuthor: "Laura Barragán (pexels)",
+    location: " Agra, India",
+  },
+  {
+    key: 1,
+    name: "Great Wall of China",
+    image:
+      "https://images.pexels.com/photos/13560534/pexels-photo-13560534.jpeg?cs=srgb&dl=pexels-tonynojmansk-13560534.jpg&fm=jpg",
+    imageAuthor: "TonyNojmanSK (pexels)",
+    location: "China",
+  },
+  {
+    key: 2,
+    name: "Chichén Itzá",
+    image:
+      "https://images.pexels.com/photos/3290068/pexels-photo-3290068.jpeg?cs=srgb&dl=pexels-alex-azabache-3290068.jpg&fm=jpg",
+    imageAuthor: "Alex Azabache (pexels)",
+    location: "Yucatan, Mexico",
+  },
+  {
+    key: 3,
+    name: "Petra",
+    image:
+      "https://images.pexels.com/photos/7487334/pexels-photo-7487334.jpeg?cs=srgb&dl=pexels-salameh-jarajreh-7487334.jpg&fm=jpg",
+    imageAuthor: "Salameh Jarajreh (pexels)",
+    location: "Petra, Jordan",
+  },
+  {
+    key: 4,
+    name: "Machu Picchu",
+    image:
+      "https://images.pexels.com/photos/90597/pexels-photo-90597.jpeg?cs=srgb&dl=pexels-amanda-kerr-90597.jpg&fm=jpg",
+    imageAuthor: "Amanda Kerr (pexels)",
+    location: "Cuzco, Peru",
+  },
+  {
+    key: 5,
+    name: "Christ the Redeemer",
+    image:
+      "https://images.pexels.com/photos/13765377/pexels-photo-13765377.jpeg?cs=srgb&dl=pexels-marcos-vinicius-scheidt-13765377.jpg&fm=jpg",
+    imageAuthor: "Marcos Vinicius Scheidt (pexels)",
+    location: "Rio de Janeiro, RJ, Brasil",
+  },
+  {
+    key: 6,
+    name: "Colosseum",
+    image:
+      "https://images.pexels.com/photos/1701595/pexels-photo-1701595.jpeg?cs=srgb&dl=pexels-khalid-ait-sayh-1701595.jpg&fm=jpg",
+    imageAuthor: "khalid ait sayh (pexels)",
+    location: "Rome",
+  },
+];
