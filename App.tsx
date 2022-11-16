0dd5cdd7909d76ce08d04ab16adf7a276fe3abcc
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-11-16 13:27:53 +0530

    stacked animation done

--- App.tsx
+++ App.tsx
@@ -1,107 +1,110 @@
 import {
   Dimensions,
   FlatList,
-  Image,
   SafeAreaView,
   StyleSheet,
   View,
 } from "react-native";
+import React, { useState } from "react";
 import {
   Directions,
   FlingGestureHandler,
   GestureHandlerRootView,
   State,
 } from "react-native-gesture-handler";
-import { useSharedValue } from "react-native-reanimated";
 import { SEVEN_WONDERS } from "./data/SevenWonders";
+import CarouselSlide from "./components/CarouselSlide";
+import {
+  useDerivedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
 
-const { width, height } = Dimensions.get("window");
-const SLIDE_HEIGHT = height * 0.6;
-const SLIDE_WIDTH = width * 0.75;
-export default function App() {
-  const currentSlideIndex = useSharedValue(0);
-
+const App = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const activeIndex = useDerivedValue(() => withSpring(currentSlide));
+  const opacityValue = useDerivedValue(() => withTiming(currentSlide));
+  const onHandlerStateChangeLeft = () => {
+    "worklet";
+    if (currentSlide === SEVEN_WONDERS.length - 1) {
+      return;
+    }
+    setCurrentSlide((prev) => prev + 1);
+  };
+  const onHandlerStateChangeRight = () => {
+    "worklet";
+    if (currentSlide === 0) {
+      return;
+    }
+    setCurrentSlide((prev) => prev - 1);
+  };
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <FlingGestureHandler
+        key={"left"}
+        direction={Directions.LEFT}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.END) {
+            onHandlerStateChangeLeft();
+          }
+        }}
+      >
         <FlingGestureHandler
-          key={"left"}
-          direction={Directions.LEFT}
+          key={"rigth"}
+          direction={Directions.RIGHT}
           onHandlerStateChange={({ nativeEvent }) => {
             if (nativeEvent.state === State.END) {
+              onHandlerStateChangeRight();
             }
           }}
         >
-          <FlingGestureHandler
-            key={"right"}
-            direction={Directions.RIGHT}
-            onHandlerStateChange={({ nativeEvent }) => {
-              if (nativeEvent.state === State.END) {
-              }
-            }}
-          >
-            <View style={styles.container}>
-              <FlatList
-                data={SEVEN_WONDERS}
-                keyExtractor={(item) => String(item.key)}
-                horizontal
-                scrollEnabled={false}
-                removeClippedSubviews={false}
-                contentContainerStyle={{
-                  flex: 1,
-                  justifyContent: "center",
-                  padding: 20,
-                }}
-                CellRendererComponent={({
-                  item,
-                  index,
-                  children,
+          <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+              data={SEVEN_WONDERS}
+              keyExtractor={({ key }) => String(key)}
+              horizontal
+              inverted
+              contentContainerStyle={{
+                flex: 1,
+                justifyContent: "center",
+                padding: 20,
+                marginTop: 50,
+              }}
+              scrollEnabled={false}
+              removeClippedSubviews={false}
+              CellRendererComponent={({
+                item,
+                index,
+                children,
+                style,
+                ...props
+              }) => {
+                const newStyle = [
                   style,
-                  ...props
-                }) => {
-                  const newStyle = [
-                    style,
-                    { zIndex: SEVEN_WONDERS.length - index },
-                  ];
-                  return (
-                    <View style={newStyle} index={index} {...props}>
-                      {children}
-                    </View>
-                  );
-                }}
-                renderItem={({ item, index }) => {
-                  return (
-                    <View style={styles.slideContainer}>
-                      <Image
-                        source={{ uri: item.image }}
-                        style={styles.slideImage}
-                      />
-                    </View>
-                  );
-                }}
-              />
-            </View>
-          </FlingGestureHandler>
+                  { zIndex: SEVEN_WONDERS.length - index },
+                ];
+                return (
+                  <View style={newStyle} index={index} {...props}>
+                    {children}
+                  </View>
+                );
+              }}
+              renderItem={({ item, index }) => {
+                return (
+                  <CarouselSlide
+                    index={index}
+                    activeIndex={activeIndex}
+                    opacityValue={opacityValue}
+                    item={item}
+                  />
+                );
+              }}
+            />
+          </SafeAreaView>
         </FlingGestureHandler>
-      </GestureHandlerRootView>
-    </SafeAreaView>
+      </FlingGestureHandler>
+    </GestureHandlerRootView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  slideContainer: {
-    position: "absolute",
-    left: -SLIDE_WIDTH / 2,
-  },
-  slideImage: {
-    height: SLIDE_HEIGHT,
-    width: SLIDE_WIDTH,
-    borderRadius: 15,
-  },
-});
+export default App;
--- components/CarouselSlide.tsx
+++ components/CarouselSlide.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, Image, StyleSheet } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
 import React from "react";
 import { Wonder } from "../data/SevenWonders";
 import Animated, {
@@ -11,39 +11,32 @@ interface CarouselSlideProps {
   index: number;
   item: Wonder;
   activeIndex: Animated.SharedValue<number>;
+  opacityValue: Animated.SharedValue<number>;
 }
 
 const { width } = Dimensions.get("window");
-
-const SLIDE_WIDTH = width * 0.65;
-const SLIDE_HEIGHT = SLIDE_WIDTH * 1.7;
-const OFFSET_SPACING = 10;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
 
 const CarouselSlide: React.FC<CarouselSlideProps> = ({
   index,
   item,
   activeIndex,
+  opacityValue,
 }) => {
   const reanimatedCarouselSlideStyle = useAnimatedStyle(() => {
     const inputRange = [index - 1, index, index + 1];
     const translateX = interpolate(
       activeIndex.value,
       inputRange,
-      [50, 0, -100],
-      Extrapolate.CLAMP
+      [50, 0, -100]
     );
-    const scale = interpolate(
-      activeIndex.value,
-      inputRange,
-      [0.8, 1, 1.3],
-      Extrapolate.CLAMP
-    );
-    const opacity = interpolate(
-      activeIndex.value,
-      inputRange,
-      [0.6, 1, 0],
-      Extrapolate.CLAMP
-    );
+    const scale = interpolate(activeIndex.value, inputRange, [0.8, 1, 1.3]);
+    const opacity = interpolate(opacityValue.value, inputRange, [
+      1 - 1 / 3,
+      1,
+      0,
+    ]);
     return {
       opacity,
       transform: [{ translateX }, { scale }],
@@ -63,11 +56,11 @@ export default CarouselSlide;
 const styles = StyleSheet.create({
   slideContainer: {
     position: "absolute",
-    left: -SLIDE_WIDTH / 2,
+    left: -ITEM_WIDTH / 2,
   },
   slideImage: {
-    width: SLIDE_WIDTH,
-    height: SLIDE_HEIGHT,
-    borderRadius: 20,
+    width: ITEM_WIDTH,
+    height: ITEM_HEIGHT,
+    borderRadius: 14,
   },
 });
