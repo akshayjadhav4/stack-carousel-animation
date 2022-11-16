e94154e69723bbc9b40eb8980c56e1ed5493049c
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-11-16 12:22:07 +0530

    wip

--- App.tsx
+++ App.tsx
@@ -1,92 +1,90 @@
-import { Dimensions, FlatList, StyleSheet, View } from "react-native";
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from "react-native";
 import {
   Directions,
   FlingGestureHandler,
   GestureHandlerRootView,
   State,
 } from "react-native-gesture-handler";
-import { useSharedValue, withSpring } from "react-native-reanimated";
-import CarouselSlide from "./components/CarouselSlide";
+import { useSharedValue } from "react-native-reanimated";
 import { SEVEN_WONDERS } from "./data/SevenWonders";
 
-const { width } = Dimensions.get("window");
-
-const SLIDE_WIDTH = width * 0.8;
-const SLIDE_HEIGHT = SLIDE_WIDTH * 1.6;
-const OFFSET_SPACING = 10;
-
+const { width, height } = Dimensions.get("window");
+const SLIDE_HEIGHT = height * 0.6;
+const SLIDE_WIDTH = width * 0.75;
 export default function App() {
-  const activeIndex = useSharedValue(0);
+  const currentSlideIndex = useSharedValue(0);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <FlingGestureHandler
-        key={"left"}
-        direction={Directions.LEFT}
-        onHandlerStateChange={({ nativeEvent }) => {
-          if (nativeEvent.state === State.END) {
-            if (Math.round(activeIndex.value) === SEVEN_WONDERS.length - 1) {
-              return;
-            }
-            activeIndex.value = withSpring(Math.round(activeIndex.value + 1));
-          }
-        }}
-      >
+    <SafeAreaView style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         <FlingGestureHandler
-          key={"rigth"}
-          direction={Directions.RIGHT}
+          key={"left"}
+          direction={Directions.LEFT}
           onHandlerStateChange={({ nativeEvent }) => {
             if (nativeEvent.state === State.END) {
-              if (Math.round(activeIndex.value) === 0) {
-                return;
-              }
-              activeIndex.value = withSpring(Math.round(activeIndex.value - 1));
             }
           }}
         >
-          <View style={styles.container}>
-            <FlatList
-              data={SEVEN_WONDERS}
-              horizontal
-              inverted
-              contentContainerStyle={{
-                flex: 1,
-                justifyContent: "center",
-                padding: OFFSET_SPACING * 2,
-              }}
-              scrollEnabled={false}
-              removeClippedSubviews={false}
-              keyExtractor={(item) => String(item.key)}
-              CellRendererComponent={({
-                item,
-                index,
-                children,
-                style,
-                ...props
-              }) => {
-                const newStyle = [
+          <FlingGestureHandler
+            key={"right"}
+            direction={Directions.RIGHT}
+            onHandlerStateChange={({ nativeEvent }) => {
+              if (nativeEvent.state === State.END) {
+              }
+            }}
+          >
+            <View style={styles.container}>
+              <FlatList
+                data={SEVEN_WONDERS}
+                keyExtractor={(item) => String(item.key)}
+                horizontal
+                scrollEnabled={false}
+                removeClippedSubviews={false}
+                contentContainerStyle={{
+                  flex: 1,
+                  justifyContent: "center",
+                  padding: 20,
+                }}
+                CellRendererComponent={({
+                  item,
+                  index,
+                  children,
                   style,
-                  { zIndex: SEVEN_WONDERS.length - index },
-                ];
-                return (
-                  <View style={newStyle} index={index} {...props}>
-                    {children}
-                  </View>
-                );
-              }}
-              renderItem={({ item, index }) => {
-                return (
-                  <CarouselSlide
-                    index={index}
-                    item={item}
-                    activeIndex={activeIndex}
-                  />
-                );
-              }}
-            />
-          </View>
+                  ...props
+                }) => {
+                  const newStyle = [
+                    style,
+                    { zIndex: SEVEN_WONDERS.length - index },
+                  ];
+                  return (
+                    <View style={newStyle} index={index} {...props}>
+                      {children}
+                    </View>
+                  );
+                }}
+                renderItem={({ item, index }) => {
+                  return (
+                    <View style={styles.slideContainer}>
+                      <Image
+                        source={{ uri: item.image }}
+                        style={styles.slideImage}
+                      />
+                    </View>
+                  );
+                }}
+              />
+            </View>
+          </FlingGestureHandler>
         </FlingGestureHandler>
-      </FlingGestureHandler>
-    </GestureHandlerRootView>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
@@ -95,14 +93,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-    paddingVertical: 20,
-  },
-  slideImage: {
-    width: SLIDE_WIDTH,
-    height: SLIDE_HEIGHT,
+    marginTop: 20,
   },
   slideContainer: {
     position: "absolute",
     left: -SLIDE_WIDTH / 2,
+  },
+  slideImage: {
+    height: SLIDE_HEIGHT,
+    width: SLIDE_WIDTH,
+    borderRadius: 15,
   },
 });
